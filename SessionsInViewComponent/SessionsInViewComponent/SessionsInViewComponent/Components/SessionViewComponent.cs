3a0cf261e96066af933034d1db7bc380c6ced85f
Dr. Shuja ur Rehman Baig <shujamughal@gmail.com>
2023-08-02 01:03:25 +0500

    sessions in view components

---
+++ SessionsInViewComponent/SessionsInViewComponent/SessionsInViewComponent/Components/SessionViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SessionsInViewComponent.Components
+{
+    public class SessionViewComponent : ViewComponent
+    {
+        private readonly IHttpContextAccessor _httpContext;
+        public SessionViewComponent(IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            object cartItems = _httpContext.HttpContext.Session.GetString("cartItems");
+            if (cartItems == null)
+            {
+                _httpContext.HttpContext.Session.SetString("cartItems", "1");
+                cartItems = "1";
+            }
+            else
+            {
+                var items = int.Parse(cartItems.ToString());
+                items++;
+                _httpContext.HttpContext.Session.SetString("cartItems", items.ToString());
+            }
+            return View(cartItems);
+        }
+    }
+}
---
+++ SessionsInViewComponent/SessionsInViewComponent/SessionsInViewComponent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SessionsInViewComponent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
